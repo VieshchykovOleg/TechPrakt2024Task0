885da6ade17a48df04d6df1abd40aa066153bd82
olegveshchykov <ipz222_vom@student.ztu.edu.ua>
2024-01-28 15:41:34 +0200

    Додано С# проект для обчислення суми двох чисел,а також переконано що gitignore спрацював

---
+++ ConsoleSum/ConsoleSum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть перше число:");
+            if (double.TryParse(Console.ReadLine(), out double num1))
+            {
+                Console.Write("Введіть друге число:");
+                if (double.TryParse(Console.ReadLine(), out double num2))
+                {
+                    double sum = num1 + num2;
+                    Console.WriteLine($"Сума двох чисел: {sum}");
+                }
+                else
+                {
+                    Console.WriteLine("Помилка. Введіть коректне друге число.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Помилка. Введіть коректне перше число.");
+            }
+
+        }
+    }
+}
